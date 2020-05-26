2057b053e5550cf1c0a9ddf49b4e4223eaa3ae17
CrisFiss <crs.hdz.s@gmail.com>
2020-05-25 20:24:17 -0500

    Lenguaje Xaml

--- App/App/App/App.xaml.cs
+++ App/App/App/App.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace App
 {
     public partial class App : Application
---
+++ App/App/App/Literals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    internal class Literals
+    {
+        public static string Message = "Hola a todos lml";
+    }
+}
