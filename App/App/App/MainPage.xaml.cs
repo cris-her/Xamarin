f28deed393f5af22932c3657383d5fb75c949ecb
CrisFiss <crs.hdz.s@gmail.com>
2020-05-25 19:47:33 -0500

    Propiedades enlazables y adjuntas

--- App/App/App/MainPage.xaml.cs
+++ App/App/App/MainPage.xaml.cs
@@ -16,6 +16,42 @@ namespace App
         public MainPage()
         {
             InitializeComponent();
+            //etiqueta.TextColor = Color.Blue;
+            //var x = etiqueta.Width;
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            //etiqueta.TextColor = Color.Blue;
+            /*if((Color)etiqueta.GetValue(Label.TextColorProperty)== Color.Red)
+            {
+                etiqueta.SetValue(Label.TextColorProperty, Color.Blue);
+            } else
+            {
+                etiqueta.SetValue(Label.TextColorProperty, Color.Red);
+            }*/
+            
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            /*var column = (int)button.GetValue(Grid.ColumnProperty);
+            if (column == 0)
+            {
+                button.SetValue(Grid.ColumnProperty, 1);
+            } else
+            {
+                button.SetValue(Grid.ColumnProperty, 0);
+            }*/
+
+            if (Grid.GetColumn(button) == 0)
+            {
+                Grid.SetColumn(button, 1);
+            } else
+            {
+                Grid.SetColumn(button, 0);
+            }
         }
     }
 }
